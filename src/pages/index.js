4060e0b0720367d865f28c08f27483d3dc1804a0
Bruno Chirelli <brunochirelli@gmail.com>
2020-08-01 14:04:08 -0300

    clean the initial boilerplate

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,28 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import { Typography } from '@material-ui/core';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+    <Layout>
+        <SEO title="Home" />
+        <Typography variant="h4" component="h1">
+            Calories Calculator
+        </Typography>
+        <Typography>Welcome to your new Gatsby site.</Typography>
+        <form>
+            <label htmlFor="male">
+                <input type="radio" name="male" id="male" />
+                Male
+            </label>
+            <label htmlFor="female">
+                <input type="radio" name="female" id="female" />
+                Female
+            </label>
+        </form>
+    </Layout>
+);
 
-export default IndexPage
+export default IndexPage;
