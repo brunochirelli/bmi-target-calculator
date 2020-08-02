6f505a4d413d4b79eadb866fac03ae2a6c30f7b7
Bruno Chirelli <brunochirelli@gmail.com>
2020-08-02 08:34:21 -0300

    remove unused instance of material-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,6 @@ module.exports = {
                 icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
             },
         },
-        `gatsby-theme-material-ui`,
         `gatsby-plugin-material-ui`,
         `gatsby-plugin-styled-components`,
     ],
