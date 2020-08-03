848f061a8632d545abbdf668690445f36bfba4ea
Bruno Chirelli <brunochirelli@gmail.com>
2020-08-03 16:34:04 -0300

    add new header boilerplate

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,15 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Typography, Container } from '@material-ui/core';
+import { Typography, Container, Box } from '@material-ui/core';
 
-const Header = ({ siteTitle }) => (
-    <Container component="header">
-        <Typography variant="h4" component="h1">
-            Calories Calculator
-        </Typography>
-        <Typography paragraph>Get your Basal Metabolic Rate and BMI.</Typography>
-    </Container>
+const Header = () => (
+    <Box component="header" bgcolor="black" color="white" paddingTop="2.5rem" paddingBottom="1rem">
+        <Container>
+            <Typography variant="h4" component="h1">
+                Calories Calculator
+            </Typography>
+        </Container>
+    </Box>
 );
-
-Header.propTypes = {
-    siteTitle: PropTypes.string,
-};
-
-Header.defaultProps = {
-    siteTitle: ``,
-};
 
 export default Header;
