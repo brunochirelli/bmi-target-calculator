e3d9efc38039f57f0dc295e7351d8040a73ae9f7
Bruno <brunochirelli@gmail.com>
2020-08-21 11:08:23 -0300

    update createMuiTheme path

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,9 +1,9 @@
-import { createMuiTheme } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core/styles';
 
 export const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: '#ff0000',
-        },
+  palette: {
+    primary: {
+      main: '#ff0000',
     },
+  },
 });
